d322943a95ad18918f53c644080f08381df34285
Fernando <fernandoamz148@gmail.com>
2021-05-31 17:10:58 -0500

    Fix sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210531182720) do
+ActiveRecord::Schema.define(version: 20210531220745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,11 +21,11 @@ ActiveRecord::Schema.define(version: 20210531182720) do
   end
 
   create_table "shouts", force: :cascade do |t|
-    t.integer  "user_id"
+    t.integer  "user_id",      null: false
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.string   "content_type"
-    t.integer  "content_id"
+    t.string   "content_type", null: false
+    t.integer  "content_id",   null: false
     t.index ["content_type", "content_id"], name: "index_shouts_on_content_type_and_content_id", using: :btree
     t.index ["user_id"], name: "index_shouts_on_user_id", using: :btree
   end
